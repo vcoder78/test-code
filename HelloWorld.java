2464dcabfab49c7d55656a262d02eebdbdaf3530
vcoder78 <vcoder78@gmail.com>
2024-01-12 00:16:24 +0530

    update javacode

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
+import java.lang.annotation.*;
+
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.print("helloworld");
+        System.out.print("helloworld\n");
 
     }
 }
